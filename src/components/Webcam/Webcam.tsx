ad37649eb3e5b97c680fc1332f9f2c479e698b02
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-10-22 05:42:11 +0800

    trying to make peer js server work

--- src/components/Webcam/Webcam.tsx
+++ src/components/Webcam/Webcam.tsx
@@ -1,10 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Webcam from 'react-webcam';
 
 export const WebcamComponent = ({ videoRef }: any) => {
   const webcamRef = React.useRef(null);
 
   console.log(webcamRef.current);
-
+  useEffect(() => {
+    (webcamRef.current! as any).video.srcObject = videoRef;
+  });
   return <Webcam ref={webcamRef}/>;
 };
