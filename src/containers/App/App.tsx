224408275793fc5d620060a2524821ed4dbc4fcd
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-10-21 23:02:20 +0800

    commit

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,9 +1,7 @@
-import Peer from 'peerjs';
-import Webcam from 'react-webcam';
+
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
-import { WebcamComponent } from '../../components/Webcam/Webcam';
 import Routes from '../../components/Routes/Routes';
 
 // import {uuid} from '../../uuid';
