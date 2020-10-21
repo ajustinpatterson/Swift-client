5f727e651e8b212fa63d03291dcd2a79eea8fc4c
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-10-22 02:53:14 +0800

    trying to make peer js server work

--- src/socket-context.js
+++ src/socket-context.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 import React from 'react';
 
-export const socket = io('ws://fathomless-eyrie-92787.herokuapp.com', {transports: ['websocket']}); //connects to server
+export const socket = io('wss://fathomless-eyrie-92787.herokuapp.com', {transports: ['websocket']}); //connects to server
 
 export const SocketContext = React.createContext(
   socket
