d0b675ebcf2ba14ab054f831ad21396914d4e7c1
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-10-22 19:10:04 +0800

    trying to make the audio works

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -2,51 +2,30 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { useHistory } from 'react-router-dom';
 import { GET_USERS } from '../../graphql/Queries';
-
-export default function UserProfile (): JSX.Element {
-
-  const { data, loading, error }  = useQuery(GET_USERS);
+import './UserProfile.scss';
+export default function UserProfile(): JSX.Element {
+  const { data, loading, error } = useQuery(GET_USERS);
   const history = useHistory();
-
   const handleClick = () => {
     history.push('/usersettings');
   };
-
   return (
-    <div>
-
-      <div>
-        Hello {data?.getUsers[0]?.name}
+    <div className="container">
+      <div className="title">Hello, {data?.getUsers[0]?.name}</div>
+      <div className="user-info">
+        <div className="profile-pic">
+          <img src={data?.getUsers[0]?.imageUrl} />
+        </div>
+        <div className="details">
+          <div>Name: {data?.getUsers[0]?.name}</div>
+          <div>Email: {data?.getUsers[0]?.email}</div>
+          <div>{data?.getUsers[0]?.bio}</div>
+          <div>{data?.getUsers[0]?.status}</div>
+          <button className="btn" onClick={handleClick}>
+            Edit profile
+          </button>
+        </div>
       </div>
-
-      <div>
-        <div>
-          <img src={data?.getUsers[0]?.imageUrl} width="500"/>
-        </div>
-
-        <div>
-          {data?.getUsers[0]?.name}
-        </div>
-
-        <div>
-        {data?.getUsers[0]?.email}
-        </div>
-
-        <div>
-        {data?.getUsers[0]?.bio}
-        </div>
-
-        <div>
-          {data?.getUsers[0]?.status}
-        </div>
-
-        <button onClick={handleClick}>
-          Edit profile
-        </button>
-
-      </div>
-
     </div>
-  )
-};
-
+  );
+}
--- src/components/UserSettings/UserSettings.tsx
+++ src/components/UserSettings/UserSettings.tsx
@@ -3,22 +3,19 @@ import { useMutation, useQuery } from '@apollo/client';
 import { GET_USERS } from '../../graphql/Queries';
 import { UPDATE_USER } from '../../graphql/Mutations';
 import { User } from './User';
-
+import './UserSettings.scss';
 const UserSettings = (): JSX.Element => {
-
-  const { data, loading, error }  = useQuery(GET_USERS);
-  const [ updateUserInfo ]  = useMutation<{updateUser: User}>(UPDATE_USER);
-
-  const [ imageFile, setImageFile ] = useState<null | File>(null);
-  const [ user, setUser ] = useState<User>({
+  const { data, loading, error } = useQuery(GET_USERS);
+  const [updateUserInfo] = useMutation<{ updateUser: User }>(UPDATE_USER);
+  const [imageFile, setImageFile] = useState<null | File>(null);
+  const [user, setUser] = useState<User>({
     _id: '',
     name: '',
     email: '',
     bio: '',
     imageUrl: '',
-    status: ''
+    status: '',
   });
-
   useEffect(() => {
     setUser({
       _id: data?.getUsers[0]?._id,
@@ -26,19 +23,17 @@ const UserSettings = (): JSX.Element => {
       email: data?.getUsers[0]?.email,
       bio: data?.getUsers[0]?.bio,
       imageUrl: data?.getUsers[0]?.imageUrl,
-      status: data?.getUsers[0]?.status
-    })
+      status: data?.getUsers[0]?.status,
+    });
   }, [data]);
-
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const stateKey = event.currentTarget.name;
     const newValue = event.currentTarget.value;
-    setUser(prevState => ({
+    setUser((prevState) => ({
       ...prevState,
-      [stateKey]: newValue
+      [stateKey]: newValue,
     }));
   };
-
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
     const userDetails: User = {
@@ -46,19 +41,22 @@ const UserSettings = (): JSX.Element => {
       name: user.name,
       email: user.email,
       bio: user.bio,
-      status: user.status
-    }
+      status: user.status,
+    };
     if (imageFile) {
       const data = new FormData();
       data.append('file', imageFile);
       data.append('upload_preset', 'jfoqugj1');
-      data.append('api_key', 'process.env.NODE_ENV.REACT_APP_CLOUDINARY_API_KEY');
-      const res = await fetch (
+      data.append(
+        'api_key',
+        'process.env.NODE_ENV.REACT_APP_CLOUDINARY_API_KEY',
+      );
+      const res = await fetch(
         'https://api.cloudinary.com/v1_1/dpyiqv7ej/image/upload',
         {
           method: 'POST',
           body: data,
-        }
+        },
       );
       const upload = await res.json();
       userDetails.imageUrl = upload.url;
@@ -66,94 +64,80 @@ const UserSettings = (): JSX.Element => {
     updateUserInfo({
       variables: {
         userDetails: userDetails,
-      }
+      },
     });
   };
-
   const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.currentTarget.files;
     if (file && file[0]) {
       setImageFile(file[0]);
     }
   };
-
   return (
     <div className="user-settings-container">
-
-      <div>
-        User Settings
-      </div>
-
-      <form
-        className="form-user-settings"
-        onSubmit={handleSubmit}
-      >
-        <div>
-          <img src={user.imageUrl} alt="user_image" width="300"/>
+      <form className="form-user-settings" onSubmit={handleSubmit}>
+        <div className="photo-section">
+          <div className="profile-photo">
+            <img src={user.imageUrl} alt="user_image" />
+          </div>
+          <div className="photo-upload">
+            <h2>Upload New Profile Image</h2>
+            <input
+              name="file"
+              type="file"
+              placeholder="Upload a new profile image"
+              onChange={uploadImage}
+            />
+          </div>
         </div>
-        <div>
-          <h2>Upload New Profile Image</h2>
+        <div className="settings-cntnr">
+          <label htmlFor="name">Name</label>
           <input
-            name="file"
-            type="file"
-            placeholder="Upload a new profile image"
-            onChange={uploadImage}
-          />
+            className="field"
+            id="name"
+            placeholder={user.name}
+            name="name"
+            type="text"
+            value={user.name}
+            onChange={handleChange}
+          ></input>
+          <label htmlFor="email">Email</label>
+          <input
+            className="field"
+            id="email"
+            placeholder={user.email}
+            name="email"
+            type="text"
+            value={user.email}
+            onChange={handleChange}
+          ></input>
+          <label htmlFor="bio">Bio</label>
+          <input
+            className="field"
+            id="bio"
+            placeholder={user.bio}
+            name="bio"
+            type="text"
+            value={user.bio}
+            onChange={handleChange}
+          ></input>
+          <label htmlFor="status">Status</label>
+          <input
+            className="field"
+            id="status"
+            placeholder={user.status}
+            name="status"
+            type="text"
+            value={user.status}
+            onChange={handleChange}
+          ></input>
+          <button className="btn" type="submit">
+            Save Changes
+          </button>
         </div>
-
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          placeholder={user.name}
-          name="name"
-          type="text"
-          value={user.name}
-          onChange={handleChange}
-        >
-        </input>
-
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          placeholder={user.email}
-          name="email"
-          type="text"
-          value={user.email}
-          onChange={handleChange}
-        >
-        </input>
-
-        <label htmlFor="bio">Bio</label>
-        <input
-          id="bio"
-          placeholder={user.bio}
-          name="bio"
-          type="text"
-          value={user.bio}
-          onChange={handleChange}
-        >
-        </input>
-
-        <label htmlFor="status">Status</label>
-        <input
-          id="status"
-          placeholder={user.status}
-          name="status"
-          type="text"
-          value={user.status}
-          onChange={handleChange}
-        >
-        </input>
-
-       <button
-        type="submit"
-       >
-         Save Changes
-       </button>
-
       </form>
     </div>
-  )
+  );
 };
-
 export default UserSettings;
+
